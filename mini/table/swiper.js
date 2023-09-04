2040aefbf379251d379c585cb2c637c3ac6c50c0
Leexiaop <282191344@qq.com>
2023-09-04 19:45:46 +0800

    da

--- mini/table/swiper.js
+++
@@ -1,6 +0,0 @@
-[
-    'https://leexiaop.github.io/static/mini/threejs_demo/webgl_animation_multiple.png',
-    'https://leexiaop.github.io/static/mini/threejs_demo/webgl_animation_skinning_additive_blending.png',
-    'https://leexiaop.github.io/static/mini/threejs_demo/webgl_animation_skinning_morph.png',
-    'https://leexiaop.github.io/static/mini/threejs_demo/webgl_camera.png' 
-]
